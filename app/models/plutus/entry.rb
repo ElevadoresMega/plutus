b3221543bdd14327c6ed5a6dedbac96a2e7f8fd0
nicolasrsande <nicolasrsande@gmail.com>
2019-02-01 17:11:11 -0300

    register entries with user (REQUIERES MIGRATION)

--- app/models/plutus/entry.rb
+++ app/models/plutus/entry.rb
@@ -30,6 +30,8 @@ module Plutus
       belongs_to :commercial_document, :polymorphic => true
     end
 
+    belongs_to :admin_user
+
     has_many :credit_amounts, :extend => AmountsExtension, :class_name => 'Plutus::CreditAmount', :inverse_of => :entry, dependent: :destroy
     has_many :debit_amounts, :extend => AmountsExtension, :class_name => 'Plutus::DebitAmount', :inverse_of => :entry, dependent: :destroy
     has_many :credit_accounts, :through => :credit_amounts, :source => :account, :class_name => 'Plutus::Account'
